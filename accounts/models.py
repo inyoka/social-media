089acd67721e847f5881544d9fbfaf116fe5fea7
Simon <simonmarkholland@gmail.com>
2018-04-06 08:07:20 +0700

    Setup builtin django auth models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionMixin):
+
+    def __str__(self):
+        return f'@{self.username}'
