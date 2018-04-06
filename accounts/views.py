333d76dbf8de0429ed3a18606db8e772cbd4ced6
Simon <simonmarkholland@gmail.com>
2018-04-06 14:44:58 +0700

    Setup form views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-from . import forms
+from .forms import UserCreateForm
 # Create your views here.
 class SignUp(CreateView):
-    form_class = forms.UserCreateForm
+    form_class = UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
