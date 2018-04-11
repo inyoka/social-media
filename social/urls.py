b55b7b92de2de3ce3aa8b490e0a94a820908985f
Simon <simonmarkholland@gmail.com>
2018-04-11 12:29:50 +0700

    Add urls and views for test and thanks page

--- social/urls.py
+++ social/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomePage
+from .views import HomePage, TestPage, ThanksPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('test/',TestPage.as_view(), name='test'),
+    path('thanks/', ThanksPage.as_view(), name='thanks')
 ]
