54ed8b6a0844b164091a9139d5efdf5814ffae8b
Simon <simonmarkholland@gmail.com>
2018-04-20 01:37:58 +0700

    Changed th name for create group

--- groups/urls.py
+++ groups/urls.py
@@ -21,7 +21,7 @@ app_name = 'groups'
 
 urlpatterns = [
     path('', ListGroups.as_view(), name='all'),
-    path('new/', CreateGroup.as_view(), name='all'),
+    path('new/', CreateGroup.as_view(), name='create'),
     path('posts/in/<slug:slug>',SingleGroup.as_view(), name='single'),
     path('join/<slug:slug>', JoinGroup.as_view(),name='join'),
     path('leave/<slug:slug>', LeaveGroup.as_view(),name='leave'),
