76fd0942927b046e46912b278b8df0c09acd8ed8
Simon <simonmarkholland@gmail.com>
2018-04-20 01:42:09 +0700

    Fixed typos and deleted automcomplete phantoms

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 
 from django.http import Http404
 from django.views import generic
@@ -16,10 +16,10 @@ from . import forms
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-class PostList(SelectRelatedMixin):
+class PostList(SelectRelatedMixin, generic.ListView):
     """docstring for PostList."""
 
-    model = models.PostList
+    model = models.Post
     select_related = ('user','group')
     # def __init__(self, arg):
     #     super(PostList, self).__init__()
@@ -27,7 +27,7 @@ class PostList(SelectRelatedMixin):
 
 
 class UserPosts(generic.ListView)        :
-    model = modes.Post
+    model = models.Post
     template_name = 'posts/user_post_list.html'
 
     def get_queryset(self):
