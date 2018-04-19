eb547531ddb7b8a55dcea32b71c626a02530cc7e
Simon <simonmarkholland@gmail.com>
2018-04-20 01:38:57 +0700

    Fixed typos and formatted warning message

--- groups/views.py
+++ groups/views.py
@@ -5,17 +5,20 @@ from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,
 PermissionRequiredMixin)
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from groups.models import Group, GroupMember
+
+from . import models
+
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ('name','description')
     model = Group
 
 class SingleGroup(generic.DetailView):
-    model = group
+    model = Group
 
 class ListGroups(generic.ListView):
     model = Group
@@ -31,21 +34,21 @@ class JoinGroup(LoginRequiredMixin, generic.RedirectView):
         try:
             GroupMember.objects.create(user=self.request.user,group=group)
         except IntegrityError:
-            messages.warning(self.request, 'Warning already a member!')
+            messages.warning(self.request,("Warning, already a member of {}".format(group.name)))
         else:
-            messages.success(self.request,'You are now a member!')
+            messages.success(self.request,"You are now a member of the {} group.".format(group.name))
 
         return super().get(request, *args,**kwargs)
 
 class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('group:single', kwargs={'slug':self.kwargs.get('slug')})
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
 
     def get(self, request, *args, **kwargs):
 
         try:
-            meembership = models.GroupMember.object.filter(
+            membership = models.GroupMember.objects.filter(
                 user=self.request.user,
                 group__slug=self.kwargs.get('slug')
             ).get()
