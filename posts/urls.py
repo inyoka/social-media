b917b4fec3ef8ad03ca663fac41f6454a4a67106
Simon <simonmarkholland@gmail.com>
2018-04-20 01:41:35 +0700

    Called DeletePost directly

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import PostList, CreatePost, UserPosts, PostDetail
+from .views import (PostList, CreatePost, UserPosts, PostDetail,
+                    DeletePost)
 
 app_name = 'posts'
 
@@ -7,6 +8,6 @@ urlpatterns = [
     path('', PostList.as_view(), name='all'),
     path('new/', CreatePost.as_view(), name='create'),
     path('by/<username>/', UserPosts.as_view(), name='for_user'),
-    path('by/<username>/<int:pk>', PostDetail.as_view(), name='single')
-    path('delete/<int:pk>/', views.DeletePost.as_view(),name='delete')
+    path('by/<username>/<int:pk>', PostDetail.as_view(), name='single'),
+    path('delete/<int:pk>/', DeletePost.as_view(),name='delete'),
 ]
