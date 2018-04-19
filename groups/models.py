4de317db1b05eca2c57f80a59d3b5ef2a2597be1
Simon <simonmarkholland@gmail.com>
2018-04-20 01:36:25 +0700

    Fixed typos in models file

--- groups/models.py
+++ groups/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
-#import misaka
+import misaka
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-from django import templates
+from django import template
 register = template.Library()
 
 class Group(models.Model):
-    name =  models.Charfield(max_length=255, unique=True, delete=CASCADE)
-    slug = models.SlugField
+    name =  models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
     description = models.TextField(blank=True,default='')
     description_html = models.TextField(editable=False, default='', blank=True)
     members = models.ManyToManyField(User, through='GroupMember')
 
     def __str__(self):
-        return self.namespace
+        return self.name
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -33,8 +34,8 @@ class Group(models.Model):
 
 
 class GroupMember(models.Model):
-    group = model.ForeignKey(Group, related_name='memberships')
-    user = models.ForeignKey(User, related_name='user_groups')
+    group = models.ForeignKey(Group, related_name='memberships', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_groups', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
